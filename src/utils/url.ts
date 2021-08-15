75908899c29a6c844d1d2210d786323d98d4cd2d
帅得乱七八糟 <284970670@qq.com>
2021-08-15 12:20:42 +0800

    查漏补缺 开始新的进度

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,12 +3,19 @@ import { useMemo } from 'react';
 /**
  * 返回页面url中，指定键的参数值
  */
+// 这里的K 代表的是string类型。  K[] K可以代表各种各样的字符串  例如 name、personId
+// <K extends string> 是限制了泛型类型
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+  // searchParams 就包含了 url 后面所带的所有参数信息
   const [searchParams, setSearchParam] = useSearchParams();
   return [
-    useMemo(() => keys.reduce((prev: {[key in K]: string}, key: K) => {
-      return {...prev, [key]: searchParams.get(key) || ''}}
-    , {} as {[key in K]: string}), [searchParams]),
+    useMemo(
+      () => keys.reduce((prev: {[key in K]: string}, key: K) => {
+        // 这里的[key]括号打得非常巧妙，如果没有，则认为key是字符串。而非变量
+        return {...prev, [key]: searchParams.get(key) || ''}
+      }, {} as {[key in K]: string}),
+      [searchParams]
+    ),
     setSearchParam
   ] as const
 }
